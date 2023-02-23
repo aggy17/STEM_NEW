07d664f8c4e0fb4c8cedc35688ee6766ddcdc7b7
aggy17 <munyivak17@gmail.com>
2023-02-23 19:48:19 +0300

    week-3

--- Week-2/odd_even.py
+++ Week-2/odd_even.py
@@ -1,2 +1,16 @@
+#!/usr/bin/env python3
+#this is a single line comment
+#operators in python
+#name : Agnes Munyiva
+#email : munyivak17@gmail.com
+#date : 21st Feb 2023
+#file : odd_even.py
 
+#calculate odd and even numbers
+for i in range (0,100):
+    no = 10
+    if(no%2==0):
+        print("even_num")
+else:
+    print("odd_num")
 
---
+++ Week-3/assignment4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#this is a single line comment
+#operators in python
+#name : Agnes Munyiva
+#email : munyivak17@gmail.com
+#date : 23rd Feb 2023
+#file : assignment4.py
+#factorial 6
+def factorial (n):
+    for i in range(0,n):
+     fact_n = n * (n-1)
+     return fact_n
+print(factorial(6))
