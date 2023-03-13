3eabc781c3058acf00144a9de48fe1d5c1bcd397
aggy17 <munyivak17@gmail.com>
2023-03-13 11:27:37 +0300

    week5

---
+++ Week-5/javascript/index.js
@@ -0,0 +1,17 @@
+let age = 5
+let msg =""
+function add_num(num1,num2)
+{
+    return num1 + num2
+}
+
+document.getElementById("confirm.btn").onclick=function(){}
+age = document.getElementById("age").innerhtml.valueof
+if (age<=18){
+    msg="you are not allowed"
+    document.getElementById("get_in").innerHTML =msg
+}else{
+    msg="welcome"
+    document.getElementById("get_in").innerHTML =msg
+
+}
